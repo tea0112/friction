d3a495b8d0545afeeb2b7eefcb0b294569bfd08e
tea0112 <ducthaidev@gmail.com>
2023-10-10 18:01:37 +0700

    remove unnecessary

--- cmd/friction/friction_test.go
+++ cmd/friction/friction_test.go
@@ -1,18 +1,1 @@
 package main
-
-import (
-	"friction/loggers"
-	"testing"
-)
-
-func TestIsZapTrue(t *testing.T) {
-	var logger loggers.Logger
-	logger = &loggers.Zap{}
-
-	got := isZap(logger)
-	want := true
-
-	if got != want {
-		t.Errorf("got %t, want %t", got, want)
-	}
-}
